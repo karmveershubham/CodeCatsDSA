d4fa132aeae593c49622d7171bd44be6e3f75f4e
Shubham Karmveer <s.karmveer1201@gmail.com>
2024-02-02 01:59:31 +0530

    3 ms Beats 84.21% of users with C++

---
+++ 2798EmployeeTarget.cpp
@@ -0,0 +1,16 @@
+// 2798. Number of Employees Who Met the Target
+// There are n employees in a company, numbered from 0 to n - 1. Each employee i has worked for hours[i] hours in the company.
+// The company requires each employee to work for at least target hours.
+// You are given a 0-indexed array of non-negative integers hours of length n and a non-negative integer target.
+// Return the integer denoting the number of employees who worked at least target hours.
+
+
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int count=0;
+        for(int& work:hours)  
+            count+= (work>=target) ;
+        return count;
+    }
+};
