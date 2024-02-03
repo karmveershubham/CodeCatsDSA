bc10c9fad04c89615b900aa116de562891dc0cb2
Shubham Karmveer <s.karmveer1201@gmail.com>
2024-02-04 01:42:33 +0530

    Create 1480RunningSum.cpp

---
+++ 1480RunningSum.cpp
@@ -0,0 +1,13 @@
+// 1480. Running Sum of 1d Array
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+// Return the running sum of nums.
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        for(int i=1; i<nums.size(); i++){
+            nums[i]=nums[i]+nums[i-1];
+        }
+        return nums;
+    }
+};
